0f8cc27c27d4ca091b7fa431d2b76d8cdd56a3da
JApitpit <Jianpitpit@gmail.com>
2025-03-07 23:36:57 -0700

    filled ExistingKeyException

--- src/exceptions/ExistingKeyException.java
+++ src/exceptions/ExistingKeyException.java
@@ -1,6 +1,21 @@
 package exceptions;
 
-public class ExistingKeyException extends Exception
-{
+/**
+ * ExistingKeyException.java
+ * 
+ * @author Aishte Pitpit
+ */
+@SuppressWarnings( "serial" )
+public class ExistingKeyException extends Exception {
 
+    public ExistingKeyException() {
+        super("Existing key detected. Each key must be unique");
+    }
+
+    /**
+     * @param message Custom error message.
+     */
+    public ExistingKeyException(String message) {
+        super(message);
+    }
 }
